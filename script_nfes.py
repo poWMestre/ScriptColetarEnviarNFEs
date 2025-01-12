f2a6555db02fc8975f214d8e7349a4dbb5a22de4
Welder <weldermalaquiasferreira@gmail.com>
2025-01-12 08:06:00 -0400

    Adicionado um arquivo TXT que representa uma variável global, presentando se já foi enviado ou não os arquivos

--- script_nfes.py
+++ script_nfes.py
@@ -1,122 +1,138 @@
 import time
 import sys
+import os
 
-dia = time.strftime("%d", time.gmtime())
-with open("JaFoiEnviadoOsArquivos.txt", "r+", encoding="UTF-8") as file:
+caminho = ""
+dia = int(time.strftime("%d", time.gmtime()))
 
+if getattr(sys, 'frozen', False):
+    caminho = os.path.join(sys._MEIPASS, 'JaFoiEnviadoOsArquivos.txt')
+else:
+    caminho = "F:\\Nova pasta\\scriptDigisat\\JaFoiEnviadoOsArquivos.txt"
+
+with open(caminho, "r", encoding="UTF-8") as file:
     tmp = file.read()
     if tmp == "True":
-        if dia == "01":
-            file.write("False")
+        if dia >= 1 and dia < 5:
+            with open(caminho, "w", encoding="UTF-8") as file:
+                file.write("False")
         sys.exit()
+
 
 ##############################################################################
 
-"""             Pegar CNPJ e criar o caminho até as NFEs              """
+    """             Pegar CNPJ e criar o caminho até as NFEs              """
+    
+if (dia >= 5 and dia <= 15) and tmp == "False":
+    
+    import time
+    import xml.etree.ElementTree as ET
+    import re
 
-import time
-import xml.etree.ElementTree as ET
-import re
 
+    DIRETORIO_PRINCIPAL = 'C:\\DigiSat\\SuiteG6\\Servidor' 
 
-DIRETORIO_PRINCIPAL = 'C:\\DigiSat\\SuiteG6\\Servidor' 
+    DIRETORIO_PEGAR_ARQUIVO_CNPJ = DIRETORIO_PRINCIPAL+'\\info.dat'
 
-DIRETORIO_PEGAR_ARQUIVO_CNPJ = DIRETORIO_PRINCIPAL+'\\info.dat'
+    tree = ET.parse(DIRETORIO_PEGAR_ARQUIVO_CNPJ)
+    root = tree.getroot()
+    cnpj = ""
 
-tree = ET.parse(DIRETORIO_PEGAR_ARQUIVO_CNPJ)
-root = tree.getroot()
-cnpj = ""
-
-for procurar_cnpj in root.findall('Matriz'):
-    cnpj = procurar_cnpj.find('Cnpj')
-    cnpj = cnpj.text
-    break
+    for procurar_cnpj in root.findall('Matriz'):
+        cnpj = procurar_cnpj.find('Cnpj')
+        cnpj = cnpj.text
+        break
+            
+    coletar_cnpj = cnpj
         
-coletar_cnpj = cnpj
-    
-temp = re.split("[./-]", coletar_cnpj)
-cnpj = ""
-    
-for elementos in temp:
-    cnpj = cnpj+elementos
+    temp = re.split("[./-]", coletar_cnpj)
+    cnpj = ""
+        
+    for elementos in temp:
+        cnpj = cnpj+elementos
 
-ano = time.strftime("%Y", time.gmtime())
-mes = time.strftime("%m", time.gmtime())
+    ano = time.strftime("%Y", time.gmtime())
+    mes = time.strftime("%m", time.gmtime())
 
-if mes == "01":
-    mes = "12"
-    #transformo em inteiro para fazer a subtração depois volto para string
-    ano = str(int(ano)-1)
-else:
-    mes = str(int(mes)-1)
+    if mes == "01":
+        mes = "12"
+        #transformo em inteiro para fazer a subtração depois volto para string
+        ano = str(int(ano)-1)
+    else:
+        mes = str(int(mes)-1)
 
-pasta_do_mes = ano+mes
+    pasta_do_mes = ano+mes
 
-mudar_diretorio_para_salvar = DIRETORIO_PRINCIPAL+f'\\DFe\\{cnpj}\\Enviado\\NFe\\{pasta_do_mes}'
+    mudar_diretorio_para_salvar = DIRETORIO_PRINCIPAL+f'\\DFe\\{cnpj}\\Enviado\\NFe\\{pasta_do_mes}'
 
-DIRETORIO_DAS_NFE = DIRETORIO_PRINCIPAL+f'\\DFe\\{cnpj}\\Enviado\\NFe\\{pasta_do_mes}\\Autorizados'
-    
+    DIRETORIO_DAS_NFE = DIRETORIO_PRINCIPAL+f'\\DFe\\{cnpj}\\Enviado\\NFe\\{pasta_do_mes}\\Autorizados'
+        
 #######################################################################################################################
 
-"""          Zipar a pasta com as NFEs            """
+    """          Zipar a pasta com as NFEs            """
 
-from shutil import make_archive
-import os
-import sys
+    from shutil import make_archive
+    import os
+    import sys
 
-try:
-    arquivos = os.listdir(DIRETORIO_DAS_NFE)
-except Exception:
-    sys.exit()
+    try:
+        arquivos = os.listdir(DIRETORIO_DAS_NFE)
+    except Exception:
+        sys.exit()
 
-mes_da_pasta = pasta_do_mes    
+    mes_da_pasta = pasta_do_mes    
 
-os.chdir(mudar_diretorio_para_salvar)
-    
-nome_da_pasta = "NFE_"+mes_da_pasta
-make_archive(nome_da_pasta, 'zip', DIRETORIO_DAS_NFE)
+    os.chdir(mudar_diretorio_para_salvar)
+        
+    nome_da_pasta = "NFE_"+mes_da_pasta
+    make_archive(nome_da_pasta, 'zip', DIRETORIO_DAS_NFE)
 
 #######################################################################################################################
+        
+    """           Enviar as NFEs            """
 
-"""           Enviar as NFEs            """
+    from email import encoders
+    from email.message import Message
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    import stat
+    from smtplib import SMTP, SMTP_SSL
 
-from email import encoders
-from email.message import Message
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import stat
-from smtplib import SMTP, SMTP_SSL
+    CABECALHO_EMAIL = f'XML DE SAIDA - Orlibrei'
+    Body = f'SEGUE EM ANEXO OS XML DE SAIDA - Orlibrei'
 
-CABECALHO_EMAIL = f'XML DE SAIDA - Cliente'
-Body = f'SEGUE EM ANEXO OS XML DE SAIDO - Cliente'
+    remetente = "#########################"
+    senha_remetente = "###################"
+    para = "##############################"
 
-remetente = "##########################"
-senha_remetente = "####################"
-para = "###############################"
+    mensagem = MIMEMultipart()
+    mensagem['Subject'] = CABECALHO_EMAIL 
+    mensagem['To'] = para
+    mensagem['From'] = remetente
 
-mensagem = MIMEMultipart()
-mensagem['Subject'] = CABECALHO_EMAIL 
-mensagem['To'] = para
-mensagem['From'] = remetente
-
-mensagem.attach(MIMEText("SEGUE EM ANEXO OS XML DE SAIDA - CLIENTE"))
+    mensagem.attach(MIMEText("SEGUE EM ANEXO OS XML DE SAIDA - Orlibrei"))
 
 
-pasta = mudar_diretorio_para_salvar+"\\"+nome_da_pasta+".zip"
-print(pasta)
+    pasta = mudar_diretorio_para_salvar+"\\"+nome_da_pasta+".zip"
 
-with open(pasta, "rb") as file:
-    part = MIMEBase('application', 'zip')
-    part.set_payload(file.read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment', filename=f'{nome_da_pasta.split("/")[-1]}'+'.zip')
-    mensagem.attach(part)
+    with open(pasta, "rb") as file:
+        part = MIMEBase('application', 'zip')
+        part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment', filename=f'{nome_da_pasta.split("/")[-1]}'+'.zip')
+        mensagem.attach(part)
 
-raw = mensagem.as_string()
+    raw = mensagem.as_string()
 
-with SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.set_debuglevel(0)
-    smtp.login(remetente, senha_remetente)
-    smtp.sendmail(remetente, para, raw)
+    with SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.set_debuglevel(0)
+        smtp.login(remetente, senha_remetente)
+        smtp.sendmail(remetente, para, raw)
 
+################################################################################
+
+    """       Para colocar dizer que já enviou o arquivo        """
+
+    with open(caminho, "w", encoding="UTF-8") as file:
+        file.write("True")
